7260a54f9dec575c6c40e5df756a421bf88b36e8
Christian Flothmann <christian.flothmann@sensiolabs.de>
2018-06-10 15:08:22 +0200

    initialize the bundle

---
+++ src/RichModelFormsBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the RichModelFormsBundle package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@sensiolabs.de>
+ * (c) Christopher Hertel <christopher.hertel@sensiolabs.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace SensioLabs\RichModelForms;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Christian Flothmann <christian.flothmann@sensiolabs.de>
+ */
+class RichModelFormsBundle extends Bundle
+{
+}
