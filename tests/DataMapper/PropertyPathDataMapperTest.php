2fba62a847b3b80fd06d2d6013482ef77f1715d5
Christian Flothmann <christian.flothmann@sensiolabs.de>
2018-07-13 11:43:44 +0200

    switch to more expressive test fixtures

--- tests/DataMapper/PropertyPathDataMapperTest.php
+++ tests/DataMapper/PropertyPathDataMapperTest.php
@@ -16,42 +16,87 @@ namespace SensioLabs\RichModelForms\Tests\DataMapper;
 
 use PHPUnit\Framework\TestCase;
 use SensioLabs\RichModelForms\Extension\RichModelFormsTypeExtension;
-use Symfony\Component\Form\AbstractType;
+use SensioLabs\RichModelForms\Tests\Fixtures\Form\CancelSubscriptionType;
+use SensioLabs\RichModelForms\Tests\Fixtures\Form\ProductDataType;
+use SensioLabs\RichModelForms\Tests\Fixtures\Model\Product;
+use SensioLabs\RichModelForms\Tests\Fixtures\Model\Subscription;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormFactoryBuilder;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class PropertyPathDataMapperTest extends TestCase
 {
-    public function testDataIsNotMappedToFormIfTheFormIsNotMapped()
+    public function testDataIsNotMappedToFormWithoutReadPropertyPathIfTheFormIsNotMapped()
     {
-        $form = $this->createForm();
-        $form->setData(new Foo());
+        $formBuilder = $this->createFormBuilder(ProductDataType::class);
+        $formBuilder->get('name')->setMapped(false);
+        $formBuilder->get('name')->setData('A way more fancy product');
 
-        $this->assertSame('bar not mapped data', $form['barNotMapped']->getData());
-        $this->assertSame('baz not mapped data', $form['bazNotMapped']->getData());
+        $form = $formBuilder->getForm();
+        $form->setData(new Product('A fancy product'));
+
+        $this->assertSame('A way more fancy product', $form['name']->getData());
     }
 
-    public function testTheDataOptionIsMappedToTheFormIfTheDataToBeMappedIsNull()
+    public function testDataIsNotMappedToFormWithReadPropertyPathIfTheFormIsNotMapped()
     {
-        $form = $this->createForm();
+        $formBuilder = $this->createFormBuilder(CancelSubscriptionType::class);
+        $formBuilder->get('cancellation_date')->setMapped(false);
+        $formBuilder->get('cancellation_date')->setData(new \DateTimeImmutable('2019-01-07'));
+
+        $form = $formBuilder->getForm();
+        $form->setData(new Subscription(new \DateTimeImmutable('2018-07-01')));
+
+        $this->assertEquals(new \DateTimeImmutable('2019-01-07'), $form['cancellation_date']->getData());
+    }
+
+    public function testTheDataOptionIsMappedToTheFormWithoutReadPropertyPathIfTheDataToBeMappedIsNull()
+    {
+        $formBuilder = $this->createFormBuilder(ProductDataType::class);
+        $formBuilder->get('name')->setData('A way more fancy product');
+
+        $form = $formBuilder->getForm();
         $form->setData(null);
 
-        $this->assertSame('bar_data', $form['bar']->getData());
-        $this->assertSame('baz_data', $form['baz']->getData());
+        $this->assertSame('A way more fancy product', $form['name']->getData());
     }
 
-    public function testTheDataOptionIsMappedToTheFormIfTheDataToBeMappedIsTheEmptyArray()
+    public function testTheDataOptionIsMappedToTheFormWithReadPropertyPathIfTheDataToBeMappedIsNull()
     {
-        $form = $this->createForm([
+        $formBuilder = $this->createFormBuilder(CancelSubscriptionType::class);
+        $formBuilder->get('cancellation_date')->setData(new \DateTimeImmutable('2019-01-07'));
+
+        $form = $formBuilder->getForm();
+        $form->setData(null);
+
+        $this->assertEquals(new \DateTimeImmutable('2019-01-07'), $form['cancellation_date']->getData());
+    }
+
+    public function testTheDataOptionIsMappedToTheFormWithoutReadPropertyPathIfTheDataToBeMappedIsTheEmptyArray()
+    {
+        $formBuilder = $this->createFormBuilder(ProductDataType::class, null, [
             'data_class' => null,
         ]);
+        $formBuilder->get('name')->setData('A way more fancy product');
+
+        $form = $formBuilder->getForm();
         $form->setData([]);
 
-        $this->assertSame('bar_data', $form['bar']->getData());
-        $this->assertSame('baz_data', $form['baz']->getData());
+        $this->assertSame('A way more fancy product', $form['name']->getData());
+    }
+
+    public function testTheDataOptionIsMappedToTheFormWithReadPropertyPathIfTheDataToBeMappedIsTheEmptyArray()
+    {
+        $formBuilder = $this->createFormBuilder(CancelSubscriptionType::class, null, [
+            'data_class' => null,
+        ]);
+        $formBuilder->get('cancellation_date')->setData(new \DateTimeImmutable('2019-01-07'));
+
+        $form = $formBuilder->getForm();
+        $form->setData([]);
+
+        $this->assertEquals(new \DateTimeImmutable('2019-01-07'), $form['cancellation_date']->getData());
     }
 
     /**
@@ -59,7 +104,7 @@ class PropertyPathDataMapperTest extends TestCase
      */
     public function testMappingScalarDataToFormsIsRejected()
     {
-        $form = $this->createForm([
+        $form = $this->createForm(CancelSubscriptionType::class, null, [
             'data_class' => null,
         ]);
         $form->setData('foo');
@@ -67,149 +112,89 @@ class PropertyPathDataMapperTest extends TestCase
 
     public function testDataForFieldsWithoutTheReadPropertyPathOptionAreStillMappedUsingTheDecoratedDataMapper()
     {
-        $form = $this->createForm();
-        $form->setData(new Foo('foo'));
+        $form = $this->createForm(ProductDataType::class, new Product('A fancy product'));
 
-        $this->assertSame('foo', $form['bar']->getData());
+        $this->assertSame('A fancy product', $form['name']->getData());
     }
 
     public function testDataToBeMappedIsReadUsingTheReadPropertyPathOption()
     {
-        $form = $this->createForm();
-        $form->setData(new Foo(null, 'foo'));
+        $cancellationDate = new \DateTimeImmutable('2018-07-01');
+        $form = $this->createForm(CancelSubscriptionType::class, new Subscription($cancellationDate));
 
-        $this->assertSame('foo', $form['baz']->getData());
+        $this->assertEquals($cancellationDate, $form['cancellation_date']->getData());
     }
 
     public function testSubmittedDataForFieldsWithoutTheWritePropertyPathOptionAreStillMappedUsingTheDecoratedDataMapper()
     {
-        $form = $this->createForm();
-        $data = new Foo();
-        $form->setData($data);
+        $product = new Product('A fancy product');
+        $form = $this->createForm(ProductDataType::class, $product);
         $form->submit([
-            'bar' => 'submitted bar',
+            'name' => 'A way more fancy product',
         ]);
 
-        $this->assertSame('submitted bar', $data->getBar());
+        $this->assertSame('A way more fancy product', $product->getName());
     }
 
-    public function testSubmittedDataForFieldsWithTheWritePropertyPathOptionAreAreMapped()
+    public function testSubmittedDataForFieldsWithTheWritePropertyPathOptionAreMapped()
     {
-        $form = $this->createForm();
-        $data = new Foo();
-        $form->setData($data);
+        $subscription = new Subscription(new \DateTimeImmutable('2018-07-01'));
+        $form = $this->createForm(CancelSubscriptionType::class, $subscription);
         $form->submit([
-            'baz' => 'submitted baz',
-        ]);
+            'cancellation_date' => [
+                'year' => '2019',
+                'month' => '1',
+                'day' => '7',
+            ],
+         ]);
 
-        $this->assertSame('submitted baz', $data->bazGetter());
+        $this->assertEquals(new \DateTimeImmutable('2019-01-07'), $subscription->cancelledFrom());
     }
 
     public function testSubmittingNonMappedTypesDoesNotChangeTheUnderlyingData()
     {
-        $form = $this->createForm();
-        $data = new Foo();
-        $form->setData($data);
+        $product = new Product('A fancy product');
+        $formBuilder = $this->createFormBuilder(ProductDataType::class, $product);
+        $formBuilder->get('name')->setMapped(false);
+
+        $form = $formBuilder->getForm();
         $form->submit([
-            'barNotMapped' => 'submitted bar',
-            'bazNotMapped' => 'submitted baz',
+            'name' => 'A way more fancy product',
         ]);
 
-        $this->assertSame('bar not mapped', $data->getBarNotMapped());
-        $this->assertSame('baz not mapped', $data->bazNotMappedGetter());
+        $this->assertSame('A fancy product', $product->getName());
     }
 
-    private function createForm(array $options = []): FormInterface
+    public function testSubmittingNonMappedTypesWithReadPropertyPathDoesNotChangeTheUnderlyingData()
+    {
+        $subscription = new Subscription(new \DateTimeImmutable('2018-07-01'));
+
+        $formBuilder = $this->createFormBuilder(CancelSubscriptionType::class, $subscription);
+        $formBuilder->get('cancellation_date')->setMapped(false);
+
+        $form = $formBuilder->getForm();
+        $form->submit([
+            'cancellation_date' => [
+                'year' => '2019',
+                'month' => '1',
+                'day' => '7',
+            ],
+        ]);
+
+        $this->assertEquals(new \DateTimeImmutable('2018-07-01'), $subscription->cancelledFrom());
+    }
+
+    private function createFormBuilder(string $type, $data = null, array $options = []): FormBuilderInterface
     {
         $formFactory = (new FormFactoryBuilder())
             ->addTypeExtension(new RichModelFormsTypeExtension(PropertyAccess::createPropertyAccessor()))
             ->getFormFactory();
 
-        return $formFactory->create(FooType::class, null, $options);
-    }
-}
-
-class Foo
-{
-    private $bar;
-    private $baz;
-    private $barNotMapped = 'bar not mapped';
-    private $bazNotMapped = 'baz not mapped';
-
-    public function __construct(?string $bar = null, ?string $baz = null)
-    {
-        $this->bar = $bar;
-        $this->baz = $baz;
+        return $formFactory->createBuilder($type, $data, $options);
     }
 
-    public function getBar()
+    private function createForm(string $type, $data, array $options = []): FormInterface
     {
-        return $this->bar;
-    }
-
-    public function setBar($bar): void
-    {
-        $this->bar = $bar;
-    }
-
-    public function bazGetter()
-    {
-        return $this->baz;
-    }
-
-    public function bazSetter($baz): void
-    {
-        $this->baz = $baz;
-    }
-
-    public function getBarNotMapped()
-    {
-        return $this->barNotMapped;
-    }
-
-    public function setBarNotMapped($barNotMapped): void
-    {
-        $this->barNotMapped = $barNotMapped;
-    }
-
-    public function bazNotMappedGetter()
-    {
-        return $this->bazNotMapped;
-    }
-
-    public function bazNotMappedSetter($bazNotMapped): void
-    {
-        $this->bazNotMapped = $bazNotMapped;
-    }
-}
-
-class FooType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('bar')
-            ->add('baz', null, [
-                'read_property_path' => 'bazGetter',
-                'write_property_path' => 'bazSetter',
-            ])
-            ->add('barNotMapped', null, [
-                'mapped' => false,
-            ])
-            ->add('bazNotMapped', null, [
-                'mapped' => false,
-                'read_property_path' => 'bazNotMappedGetter',
-                'write_property_path' => 'bazNotMappedSetter',
-            ])
-        ;
-        $builder->get('bar')->setData('bar_data');
-        $builder->get('baz')->setData('baz_data');
-        $builder->get('barNotMapped')->setData('bar not mapped data');
-        $builder->get('bazNotMapped')->setData('baz not mapped data');
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefault('data_class', Foo::class);
+        return $this->createFormBuilder($type, $data, $options)->getForm();
     }
 }
---
+++ tests/Fixtures/Model/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the RichModelFormsBundle package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@sensiolabs.de>
+ * (c) Christopher Hertel <christopher.hertel@sensiolabs.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace SensioLabs\RichModelForms\Tests\Fixtures\Model;
+
+class Product
+{
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->setName($name);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        if ('' === $name) {
+            throw new \InvalidArgumentException('The product name must not be empty.');
+        }
+
+        $this->name = $name;
+    }
+}
