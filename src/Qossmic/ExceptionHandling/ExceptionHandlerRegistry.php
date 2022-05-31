86730f5d5b8880accfd13318f895eee762aa7255
Christian Flothmann <christian.flothmann@qossmic.com>
2022-05-31 08:49:26 +0200

    add QOSSMIC to the license information

--- src/Qossmic/ExceptionHandling/ExceptionHandlerRegistry.php
+++ src/Qossmic/ExceptionHandling/ExceptionHandlerRegistry.php
@@ -5,6 +5,7 @@
  *
  * (c) Christian Flothmann <christian.flothmann@qossmic.com>
  * (c) Christopher Hertel <mail@christopher-hertel.de>
+ * (c) QOSSMIC GmbH <info@qossmic.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
